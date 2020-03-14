67437b1462b3e13c75356fe10e29d78c604e1ec8
Theophilus Denanyoh <theodenanyoh@theophiluss-air.lan>
2020-03-14 19:42:20 -0400

    update 1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `You are a Lawyer`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
